42b4309a6637e2f8b649c9025ec8624352bd0469
Cristian Mihai <85120994+cristianmihai01@users.noreply.github.com>
2023-08-12 17:02:59 +0300

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,3 +1,34 @@
+// icons
+import {
+  HiHome,
+  HiUser,
+  HiViewColumns,
+  HiRectangleGroup,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from 'react-icons/hi2';
+
+// nav data
+export const navData = [
+  { name: 'home', path: '/', icon: <HiHome /> },
+  { name: 'about', path: '/about', icon: <HiUser /> },
+  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
+  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+  {
+    name: 'testimonials',
+    path: '/testimonials',
+    icon: <HiChatBubbleBottomCenterText />,
+  },
+  {
+    name: 'contact',
+    path: '/contact',
+    icon: <HiEnvelope />,
+  },
+];
+
+// next link
+import Link from 'next/link';
+
 //  links
 const links = [
   { name: 'home', path: '/', icon: <HiHome /> },
