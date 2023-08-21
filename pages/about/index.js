38b3620e17229b0a1aa1e89a4daa971e0996c501
Cristian Mihai <85120994+cristianmihai01@users.noreply.github.com>
2023-08-21 03:47:10 +0300

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,21 @@
+// icons
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaWordpress,
+  FaFigma,
+} from "react-icons/fa";
+
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+} from "react-icons/si";
+
+
 //  data
 const aboutData = [
   {
