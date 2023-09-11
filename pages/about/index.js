d41adc33fea8bc7d7dc7231ece91fe46f1d071c8
abeertech01 <abeer.technology@gmail.com>
2023-09-12 01:49:51 +0600

    test 3

--- pages/about/index.js
+++ pages/about/index.js
@@ -189,51 +189,51 @@ const About = () => {
     //           </motion.div>
     //         </div>
     //         {/* info */}
-    <motion.div
-      variants={fadeIn("left", 0.4)}
-      initial="hidden"
-      animate="show"
-      exit="hidden"
-      className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
-    >
-      <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-        {aboutData.map((item, itemIndex) => (
-          <div
-            key={itemIndex}
-            className={`${
-              index === itemIndex &&
-              "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
-            } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-            onClick={() => setIndex(itemIndex)}
-          >
-            {item.title}
-          </div>
-        ))}
-      </div>
-      <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
-        {aboutData[index].info.map((item, itemIndex) => (
-          <div
-            key={itemIndex}
-            className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
-          >
-            {/* title */}
-            <div className="font-light mb-2 md:mb-0">{item.title}</div>
-            <div className="hidden md:flex">-</div>
-            <div>{item.stage}</div>
-            <div className="flex gap-x-4">
-              {/* icons */}
-              {item.icons?.map((icon, itemIndex) => {
-                return (
-                  <div key={itemIndex} className="text-2xl text-white">
-                    {icon}
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-        ))}
-      </div>
-    </motion.div>
+    // <motion.div
+    //   variants={fadeIn("left", 0.4)}
+    //   initial="hidden"
+    //   animate="show"
+    //   exit="hidden"
+    //   className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
+    // >
+    <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+      {aboutData.map((item, itemIndex) => (
+        <div
+          key={itemIndex}
+          className={`${
+            index === itemIndex &&
+            "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+          } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+          onClick={() => setIndex(itemIndex)}
+        >
+          {item.title}
+        </div>
+      ))}
+    </div>
+    // <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+    //   {aboutData[index].info.map((item, itemIndex) => (
+    //     <div
+    //       key={itemIndex}
+    //       className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+    //     >
+    //       {/* title */}
+    //       <div className="font-light mb-2 md:mb-0">{item.title}</div>
+    //       <div className="hidden md:flex">-</div>
+    //       <div>{item.stage}</div>
+    //       <div className="flex gap-x-4">
+    //         {/* icons */}
+    //         {item.icons?.map((icon, itemIndex) => {
+    //           return (
+    //             <div key={itemIndex} className="text-2xl text-white">
+    //               {icon}
+    //             </div>
+    //           )
+    //         })}
+    //       </div>
+    //     </div>
+    //   ))}
+    // </div>
+    // </motion.div>
     //       </div>
     //     </div>
   )
